b821ea3dd99343cac9edec81390db6a49ca0aabb
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-06-07 22:07:15 +0200

    feat: get testing with jest to work

--- apps/todos/src/app/app.component.spec.ts
+++ apps/todos/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    });
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -26,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todos!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to todos!'
+    );
   });
 });
